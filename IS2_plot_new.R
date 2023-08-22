8f22f278d3abc579cf3d9bbc78597e1d045f7ce5
Gavin Cooper <phd@gavincooper.net>
2023-08-23 09:58:21 +1000

    Plot old vs new IS2 result

---
+++ IS2_plot_new.R
@@ -0,0 +1,6 @@
+load(here::here("scratch", "IS2_pmwg_oldscript.RData"), envir = oldrun <- new.env())
+
+load(here::here("scratch", "IS2_pmwg_getlogp.RData"), envir = newrun <- new.env())
+
+plot(sort(oldrun$tmp))
+points(sort(newrun$isamples$samples) + 1, col = "red")
