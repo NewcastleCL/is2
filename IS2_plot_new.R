00615af1dbf340c946dfc06d14e17356c44ed10b
Gavin Cooper <phd@gavincooper.net>
2023-08-23 10:00:21 +1000

    Fix plot, point to correct object in env

--- IS2_plot_new.R
+++ IS2_plot_new.R
@@ -3,4 +3,4 @@ load(here::here("scratch", "IS2_pmwg_oldscript.RData"), envir = oldrun <- new.en
 load(here::here("scratch", "IS2_pmwg_getlogp.RData"), envir = newrun <- new.env())
 
 plot(sort(oldrun$tmp))
-points(sort(newrun$isamples$samples) + 1, col = "red")
+points(sort(newrun$importance_samples$samples) + 1, col = "red")
